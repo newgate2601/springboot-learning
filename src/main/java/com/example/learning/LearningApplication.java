0024b410838d1ea2476ea0c2d8d47ef095cc2f2f
newgate2601 <newgate2601@gmail.com>
2024-09-09 01:44:13 +0700

    feature: zipkin sleuth user-service

--- src/main/java/com/example/learning/LearningApplication.java
+++ src/main/java/com/example/learning/LearningApplication.java
@@ -2,7 +2,10 @@ package com.example.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,3 +15,4 @@ public class LearningApplication {
 		SpringApplication.run(LearningApplication.class, args);
 	}
 }
+
--- src/main/java/com/example/learning/service/UserService.java
+++ src/main/java/com/example/learning/service/UserService.java
@@ -8,6 +8,7 @@ import com.example.learning.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,9 +19,13 @@ import java.util.Objects;
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final RestTemplate restTemplate;
 
     @Transactional(readOnly = true)
     public List<UserEntity> getUsers(){
+        String url = "http://localhost:8080/oke/";
+        Integer value = restTemplate.getForObject(url, Integer.class);
+        System.out.println("************ : " + value);
         List<UserEntity> userEntities = userRepository.findAll();
         return Objects.nonNull(userEntities) ? userEntities : new ArrayList<>();
     }
