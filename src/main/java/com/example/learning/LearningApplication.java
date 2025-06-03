3140cee5df95955afd0aed722e43f4f44e3f3416
newgate2601 <newgate2601@gmail.com>
2025-06-04 00:39:13 +0700

    add: oke chua

--- src/main/java/com/example/learning/LearningApplication.java
+++ src/main/java/com/example/learning/LearningApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class LearningApplication {
-
+	// oke chua
 	public static void main(String[] args)	 {
 		SpringApplication.run(LearningApplication.class, args);
 	}
